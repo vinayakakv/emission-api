a85f883d5f38801e36f318cc0e95aaa1ee4799c3
Vinayaka K V <me.vinayakakv@gmail.com>
2022-04-22 20:51:35 +0530

    Add basic driver code for routes

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,24 @@
+import { CountryController } from "./controllers/country.controller";
+import { initDb } from "./db";
 import express from "express";
+import { EmissonController } from "./controllers/emission.controller";
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+initDb();
+
+app.get("/api/country", async (req, res) => {
+  const result = await CountryController.getCountries();
+  res.json(result);
+});
+
+app.get("/api/country/:id", async (req, res) => {
+  const result = await EmissonController.getEmissions({
+    countryId: "CHE",
+    startYear: 1990,
+    endYear: 2018,
+  });
+  res.json(result);
 });
 
 app.listen(process.env.PORT || port, () => {
