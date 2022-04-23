0376b0357d2c67a21ed16087d80f96ddcfca001c
Vinayaka K V <me.vinayakakv@gmail.com>
2022-04-23 16:49:21 +0530

    Implement request validation

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,27 @@ import { CountryController } from "./controllers/country.controller";
 import { initDb } from "./db";
 import express from "express";
 import { EmissonController } from "./controllers/emission.controller";
+import z from "zod";
+import { dataset } from "./models/dataset";
 const app = express();
 const port = 3000;
 
 initDb();
+
+const requestSchema = z.object({
+  id: z
+    .string()
+    .regex(/[A-Z]{3}/, "Invalid country code, must contain 3 characters"),
+  startYear: z
+    .string()
+    .regex(/\d{4}/, "Invalid startYear")
+    .transform(year => parseInt(year)),
+  endYear: z
+    .string()
+    .regex(/\d{4}/, "Invalid endYear")
+    .transform(year => parseInt(year)),
+  data: z.array(dataset),
+});
 
 app.get("/api/country", async (req, res) => {
   const result = await CountryController.getCountries();
@@ -13,10 +30,37 @@ app.get("/api/country", async (req, res) => {
 });
 
 app.get("/api/country/:id", async (req, res) => {
+  const inputs = {
+    id: req.params.id,
+    startYear: req.query.startYear,
+    endYear: req.query.endYear,
+    data: [req.query.data].flat(),
+  };
+  console.log(inputs);
+  const parseResult = requestSchema.safeParse(inputs);
+  if (!parseResult.success) {
+    res.status(400).json(parseResult.error.errors);
+    return;
+  }
+  const { id, startYear, endYear, data } = parseResult.data;
+  const country = await CountryController.getCountry(id);
+  if (!country) {
+    res.status(403).json({ message: `Country ${id} not found` });
+    return;
+  }
+  if (startYear > endYear) {
+    res
+      .status(400)
+      .json({
+        message: `startYear ${startYear} must be less than or equal to endYear ${endYear}`,
+      });
+    return;
+  }
   const result = await EmissonController.getEmissions({
-    countryId: "CHE",
-    startYear: 1990,
-    endYear: 2018,
+    countryId: country.id,
+    startYear,
+    endYear,
+    datasets: data,
   });
   res.json(result);
 });
