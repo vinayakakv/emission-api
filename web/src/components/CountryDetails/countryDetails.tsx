fb8585847703eb52f7ee1899259d31bb029cefaa
Vinayaka K V <me.vinayakakv@gmail.com>
2022-04-24 22:45:15 +0530

    Implement chart with dummy data

--- web/src/components/CountryDetails/countryDetails.tsx
+++ web/src/components/CountryDetails/countryDetails.tsx
@@ -1,4 +1,6 @@
 import Dialog from "@reach/dialog";
+import { VictoryChart, VictoryAxis, VictoryLine } from "victory";
+import "./styles.css";
 
 type Props = {
   isOpen: boolean;
@@ -12,12 +14,20 @@ type Props = {
   };
 };
 
+const emissionHistory = [
+  { year: 1998, data: { CO2: 1 } },
+  { year: 1999, data: { CO2: 2 } },
+  { year: 2000, data: { CO2: 1 } },
+  { year: 2001, data: { CO2: 2.35 } },
+];
+
 export function CountryDetails({ isOpen, onDismiss, data }: Props) {
   const { countryId, countryName, dataset, emission, year } = data;
   return (
     <Dialog
       as="div"
       aria-label="Country Details"
+      className="dialog"
       isOpen={isOpen}
       onDismiss={onDismiss}
     >
@@ -33,6 +43,19 @@ export function CountryDetails({ isOpen, onDismiss, data }: Props) {
         </b>{" "}
         {emission !== undefined ? `${emission} kiloTonn` : "Data not available"}
       </p>
+      <VictoryChart domainPadding={30}>
+        <VictoryAxis
+          tickValues={emissionHistory.map(emission => emission.year)}
+        />
+        <VictoryAxis dependentAxis tickFormat={x => `${x}`} />
+        <VictoryLine
+          height={200}
+          data={emissionHistory}
+          x="year"
+          y={data => data.data.CO2}
+        />
+      </VictoryChart>
+
       <button onClick={onDismiss}>Okay</button>
     </Dialog>
   );
