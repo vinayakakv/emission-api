fc6a2061f8a984ad93734ef069fd3fe7085ff319
Vinayaka K V <me.vinayakakv@gmail.com>
2022-04-24 22:02:47 +0530

    Refactor color map into constants

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -103,18 +103,7 @@ function App() {
               "fill-opacity": 0.3,
               "fill-color": {
                 property: "emissionScale",
-                stops: [
-                  [0, "#3288bd"],
-                  [1, "#66c2a5"],
-                  [2, "#abdda4"],
-                  [3, "#e6f598"],
-                  [4, "#ffffbf"],
-                  [5, "#fee08b"],
-                  [6, "#fdae61"],
-                  [7, "#f46d43"],
-                  [8, "#d53e4f"],
-                  [9, "#ff001d"],
-                ],
+                stops: constants.emissionsColorMap,
               },
             }}
           />
--- web/src/constants.ts
+++ web/src/constants.ts
@@ -1,4 +1,16 @@
 export const constants = {
   mapboxToken: import.meta.env["VITE_MAPBOX_TOKEN"] || "",
   geoJsonUrl: import.meta.env["VITE_GEOJSON_URL"] || "",
+  emissionsColorMap: [
+    [0, "#3288bd"],
+    [1, "#66c2a5"],
+    [2, "#abdda4"],
+    [3, "#e6f598"],
+    [4, "#ffffbf"],
+    [5, "#fee08b"],
+    [6, "#fdae61"],
+    [7, "#f46d43"],
+    [8, "#d53e4f"],
+    [9, "#ff001d"],
+  ],
 };
